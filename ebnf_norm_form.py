544bc8751c5405808bb1ca5e2294e23bd6ca1186
Talbenxu <talbenxu@gmail.com>
2020-12-19 02:47:42 +1100

    Add the file function structure

--- ebnf_norm_form.py
+++ ebnf_norm_form.py
@@ -1,7 +1,7 @@
 import re
-def ebnf_file_reader():
+def ebnf_file_reader(filename):
     search_pattern = re.compile(r'Start:([\s\S]*)Productions:([\s\S]*)')
-    with open('demo/simple_EBNF_example.txt', 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         string = f.read()
     match_instance = search_pattern.search(string)
     if match_instance == None:
@@ -9,7 +9,7 @@ def ebnf_file_reader():
     start_symbol, production_rules = match_instance.group(1).strip(), match_instance.group(2).split(';')
     return start_symbol, production_rules
 
-def ebnf_grammar_loader(start_symbol, production_rules):
+def ebnf_grammar_loader(production_rules):
     # paser the string to dict datastructure
     grammar = dict()
     for rule in production_rules:
@@ -20,7 +20,31 @@ def ebnf_grammar_loader(start_symbol, production_rules):
         for rule in LHS.split('|'):
             rule = rule.split()
             grammar[head].append(rule)
+    return grammar
+
+# Convert every repetition * or { E } to a fresh non-terminal X and add
+# X = $\epsilon$ | X E
+def ebnf_repetition_replace(grammar):
+    return grammar
+
+# Convert every option ? [ E ] to a fresh non-terminal X and add
+# X = $\epsilon$ | E.
+# (We can convert X = A [ E ] B. to X = A E B | A B.)
+def ebnf_option_replace(grammar):
+    return grammar
+
+# Convert every group ( E ) to a fresh non-terminal X and add
+# X = E.
+def ebnf_group_replace(grammar):
+    return grammar
+
+def ebnf_bnf_convertor(filename):
+    start_symbol, production_rules = ebnf_file_reader(filename)
+    grammar = ebnf_grammar_loader(production_rules)
+    grammar = ebnf_repetition_replace(grammar)
+    grammar = ebnf_option_replace(grammar)
+    grammar = ebnf_group_replace(grammar)
     return start_symbol, grammar
 
 if __name__ == "__main__":
-    ebnf_grammar_loader(*ebnf_file_reader())
+    print(ebnf_bnf_convertor('demo/simple_EBNF_example.txt'))
