1fda49c652841dffb38301c552d0b84950058abb
Talbenxu <talbenxu@gmail.com>
2020-12-19 02:15:50 +1100

    Add the file reader for ebnf based on re

--- ebnf_norm_form.py
+++ ebnf_norm_form.py
@@ -1,7 +1,15 @@
-# Read all to as tuple in list
 import re
-production = []
-with open('demo/simple_EBNF_example.txt', 'r') as f:
-    # get production rule part get more familiar with grammar_read
-    for line in f.readlines():
-        if 
+def ebnf_file_reader():
+    search_pattern = re.compile(r'Start:([\s\S]*)Productions:([\s\S]*)')
+    with open('demo/simple_EBNF_example.txt', 'r', encoding="utf-8") as f:
+        string = f.read()
+    match_instance = search_pattern.search(string)
+    if match_instance == None:
+        raise Exception("The form of ebnf is not correct.")
+    start_symbol, production_rules = match_instance.group(1).strip(), match_instance.group(2).split(';')
+    return start_symbol, production_rules
+
+
+
+if __name__ == "__main__":
+    ebnf_file_reader()
