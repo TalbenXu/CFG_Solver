1afaf5bb21060c8453682afdfb34a077e709624f
Talbenxu <talbenxu@gmail.com>
2020-12-19 09:31:02 +1100

    Substitude the repetition with generated global int

--- ebnf_norm_form.py
+++ ebnf_norm_form.py
@@ -1,4 +1,5 @@
 import re
+new_terminal_subscript = 0
 def ebnf_file_reader(filename):
     search_pattern = re.compile(r'Start:([\s\S]*)Productions:([\s\S]*)')
     with open(filename, 'r', encoding="utf-8") as f:
@@ -22,9 +23,38 @@ def ebnf_grammar_loader(production_rules):
             grammar[head].append(rule)
     return grammar
 
+def ebnf_bracket_match(rule, i_position):
+    index = i_position
+    while index >= 0:
+        if rule[index] == "(":
+            return index
+        index -= 1
+    return Exception("Ebnf form is not correct.")
+
+def num_generator():
+    global new_terminal_subscript
+    new_terminal_subscript += 1
+    return new_terminal_subscript
 # Convert every repetition * or { E } to a fresh non-terminal X and add
 # X = $\epsilon$ | X E
 def ebnf_repetition_replace(grammar):
+    # select * position
+    for head in grammar:
+        for rule in grammar[head]:
+            i = 0
+            while i < len(rule):
+                if rule[i] == "*":
+                    X = f'X{num_generator()}'
+                    if i == 0:
+                        raise Exception('Ebnf form is not correct!')
+                    elif rule[i-1] != ")":
+                        rule[i-1:i+1] = [X]
+                    else:
+                        brack_start = ebnf_bracket_match(rule, i)
+                        rule[brack_start:i+1] = [X]
+                        print(rule)
+                        i = brack_start
+                i += 1
     return grammar
 
 # Convert every option ? [ E ] to a fresh non-terminal X and add
