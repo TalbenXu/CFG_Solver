941303195ca697f5935cc650ae244d8c7f04fd58
Talbenxu <talbenxu@gmail.com>
2020-12-21 21:25:58 +1100

    Make agenda to improve the effeiceny

---
+++ solver.py
@@ -0,0 +1,10 @@
+#!python3
+# This is the solver
+# solver graph.dot grammar.txt -3M -std
+# default DS_structure is three-dimensional matrix
+# default solver is standard cubic solver
+import sys
+
+print("hello!")
+print(sys.argv)
+
